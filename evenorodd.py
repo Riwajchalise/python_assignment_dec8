8c1b559516f2b6387997040621496fedc7e372e5
Riwajchaise <wazzree@gmail.com>
2017-12-11 11:15:24 +0545

    function to check if the given integer is even or odd

---
+++ evenorodd.py
@@ -0,0 +1,10 @@
+def f(a):
+    if a % 2 == 0:
+        print("The number you entered is even")
+
+    else:
+        print("The number you entered is odd")
+
+
+x = int(input("Enter an number"))
+f(x)
