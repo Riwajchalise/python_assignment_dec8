6081b037e26851d05be8a4664c7cc9f45c4379a8
Riwajchaise <wazzree@gmail.com>
2017-12-11 11:19:19 +0545

    Password checking function

---
+++ Password_Check.py
@@ -0,0 +1,13 @@
+password = "password123"
+
+def pas(x):
+    if x == password:
+        print ("Welcome")
+
+    else:
+        print("Access denied")
+
+
+a = str(input("enter a password"))
+pas(a)
+
